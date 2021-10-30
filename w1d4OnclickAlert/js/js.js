ffaa0fbad19c6715775158e640a719acdfaf5c7f
Mirkena <mircko2@gmail.com>
2021-10-29 22:51:50 -0400

    Create js.js

---
+++ w1d4OnclickAlert/js/js.js
@@ -0,0 +1,11 @@
+function popup(){
+
+    alert("This is me");
+}
+function display(){
+    document.getElementById("txt").innerHTML="JavaScript is the world's most popular programming language.";
+}
+function image(){
+    document.getElementById("fst").src="../w1d4OnclickAlert/image/imge.jpg";
+   
+}
