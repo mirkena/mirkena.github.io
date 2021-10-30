739ad5e938de35e57f39c0ec84938b8fcf5521e3
Mirkena <mircko2@gmail.com>
2021-10-29 22:56:19 -0400

    Update js.js

--- w1d4OnclickAlert/js/js.js
+++ w1d4OnclickAlert/js/js.js
@@ -6,6 +6,6 @@ function display(){
     document.getElementById("txt").innerHTML="JavaScript is the world's most popular programming language.";
 }
 function image(){
-    document.getElementById("fst").src="../w1d4OnclickAlert/image/imge.jpg";
+    document.getElementById("fst").src="imge.jpg";
    
 }
