57c217a6f4d8b23ac9973e973da971354bd4db46
Mirkena <mircko2@gmail.com>
2021-11-03 09:24:54 -0400

    Update js.js

--- jsTry/js.js
+++ jsTry/js.js
@@ -10,3 +10,60 @@ function getA(str){
 }
     var d=getA("heeeey");
     getA();
+    var geometry=({
+        area:function(radius){
+            return Math.PI*radius*radius;
+        },
+        circumphrance:function(radius){
+            return Math.PI*radius*2;
+        }
+       
+    
+    
+    })();
+    var x=geometry.area(3);
+    alert("HEllo");
+    alert(x);
+    alert("hello");
+var counter=(function(){
+	var privCounter=0;
+    function change(val){
+    	privCounter+=val;
+    }
+    return{
+    	increament:function(){
+        	chage(1);
+         	},
+        decreament:function(){
+        	change(-1);
+           },
+		value:function(){
+        	return privCounter;
+            }
+    }
+})();
+var counter=(function(){
+	var privCounter=0;
+    function change(val){
+    	privCounter+=val;
+    }
+    let myObject={};
+     privMethod=function(){}
+    return{
+    	increament:function(){
+        	chage(1);
+         	},
+        decreament:function(){
+        	change(-1);
+           },
+		value:function(){
+        	return privCounter;
+            }
+    }
+})();
+
+
+counter.increament();
+counter.increament();
+alert(counter.value());
+alert(counter.value);
