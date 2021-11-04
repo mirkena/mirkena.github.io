e527de23f87844a93be67febd3eafd4340b6465b
Mirkena <mircko2@gmail.com>
2021-11-04 00:36:38 -0400

    Update js.js

--- jsTry/js.js
+++ jsTry/js.js
@@ -5,65 +5,23 @@
 //     document.getElementById('myImage').src='off.gif';
 // }
 
-function getA(str){
-	return function(){alert("HAAAAAP");}
-}
-    var d=getA("heeeey");
-    getA();
-    var geometry=({
-        area:function(radius){
-            return Math.PI*radius*radius;
-        },
-        circumphrance:function(radius){
-            return Math.PI*radius*2;
-        }
+x = 1;
+var a = 5;
+var b = 10;
+var c = function(a, b, c) {
+            var x = 10;    
+           document.write(x); 
+           document.write(a);
+       var f = function(a, b, c) {
+               b = a;
+               document.write(b);
+               b = c;
+               var x = 5;
+       }    
+       f(a,b,c);
+           document.write(b);
+       } 
+           c(8,9,10);
+       document.write(b);
+       document.write(x);
        
-    
-    
-    })();
-    var x=geometry.area(3);
-    alert("HEllo");
-    alert(x);
-    alert("hello");
-var counter=(function(){
-	var privCounter=0;
-    function change(val){
-    	privCounter+=val;
-    }
-    return{
-    	increament:function(){
-        	chage(1);
-         	},
-        decreament:function(){
-        	change(-1);
-           },
-		value:function(){
-        	return privCounter;
-            }
-    }
-})();
-var counter=(function(){
-	var privCounter=0;
-    function change(val){
-    	privCounter+=val;
-    }
-    let myObject={};
-     privMethod=function(){}
-    return{
-    	increament:function(){
-        	chage(1);
-         	},
-        decreament:function(){
-        	change(-1);
-           },
-		value:function(){
-        	return privCounter;
-            }
-    }
-})();
-
-
-counter.increament();
-counter.increament();
-alert(counter.value());
-alert(counter.value);
