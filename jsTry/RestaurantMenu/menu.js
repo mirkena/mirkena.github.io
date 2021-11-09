99ac5d60ea76cae88dc793e2e16eafe8d1dec67c
Mirkena <mircko2@gmail.com>
2021-11-09 15:23:02 -0500

    Update menu.js

--- jsTry/RestaurantMenu/menu.js
+++ jsTry/RestaurantMenu/menu.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var v = false;
-    var $f, $m;
+    var f, m;
     $("button#vegOn").click(function () {
         if (v == false) {
-            $f = $(".fish").parent().parent().detach();
+            f = $(".fish").parent().parent().detach();
             $(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mushroom</em></li>");
             $(".portobello").parent().parent().addClass("veg_leaf");
             $(".meat").after("<li class='tofu'><em>Tofu</em></li>");
-            $m = $(".meat").detach();
+            m = $(".meat").detach();
             $(".tofu").parent().parent().addClass("veg_leaf");
             v = true;
         }// end if
@@ -16,10 +16,10 @@ $(document).ready(function () {
         if (v == true) {
             $(".portobello").parent().parent().removeClass("veg_leaf");
             $(".portobello").replaceWith("<li class='hamburger'>Hamburger</li>");
-            $(".menu_entrees li").first().before($f);
+            $(".menu_entrees li").first().before(f);
             $(".tofu").parent().parent().removeClass("veg_leaf");
             $(".tofu").each(function (i) {
-                $(this).after($m[i]);
+                $(this).after(  m[i]);
             });//end each
             $(".tofu").remove();
             v = false;
