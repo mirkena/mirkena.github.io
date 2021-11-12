dd9b33a9ad710571cd4ef779359c158bc5475d10
Mirkena <mircko2@gmail.com>
2021-11-12 00:12:13 -0500

    Create demo_fileServer.js

---
+++ NodeJS/fullNodeJSappln/demo_fileServer.js
@@ -0,0 +1,17 @@
+var http = require('http'); //to create server---see line 5
+var url = require('url');   //to parse url---see line 6
+var fs = require('fs');    //to read file-----see line 8
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
