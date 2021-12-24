560743a1f7e1ad8f0daada6d5f88a19271aac145
Mirkena <mircko2@gmail.com>
2021-12-24 11:34:30 -0600

    Update myfirst.js

--- NodeJS/myfirst.js
+++ NodeJS/myfirst.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var dt = require('./myFirstModule');
+var dt = require('./myfirstmodule');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The Date and Time are currently: " +dt.myDateTime);
