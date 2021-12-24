533a8ccd11c1e60d8ecb7dd4830759605fe60a29
Mirkena <mircko2@gmail.com>
2021-12-24 11:34:27 -0600

    Update demo_querystring.js

--- NodeJS/demo_querystring.js
+++ NodeJS/demo_querystring.js
@@ -4,6 +4,6 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   //code below use the variable url declared above
   var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
+  var txt = q.year + " " + q.month+" "+q.name;
   res.end(txt);
 }).listen(8080);
